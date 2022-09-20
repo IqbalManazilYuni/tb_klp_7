17ac22fb667e50632af2a3441706775e3349c184
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-09-20 10:44:10 +0700

    logbook

---
+++ app/src/main/java/id/ac/unand/tb_klp_7/DetailLogbook.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.tb_klp_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailLogbook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_logbook)
+    }
+}
