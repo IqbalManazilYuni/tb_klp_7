bbb0fbfecceab0b5dba05b21962d8153312f3178
Erlanggadn <90028936+Erlanggadn@users.noreply.github.com>
2022-09-21 18:21:29 +0700

    Profile completed

---
+++ app/src/main/java/id/ac/unand/tb_klp_7/Update_Profile.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.tb_klp_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Update_Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_profile)
+    }
+}
