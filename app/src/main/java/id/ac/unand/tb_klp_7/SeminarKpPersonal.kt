d80dfc96dc11478c3f43539433877ebc0f28381c
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-09-21 19:43:57 +0700

    layout

---
+++ app/src/main/java/id/ac/unand/tb_klp_7/SeminarKpPersonal.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.tb_klp_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeminarKpPersonal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seminar_kp_personal)
+    }
+}
