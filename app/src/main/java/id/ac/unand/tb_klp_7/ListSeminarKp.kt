4ce01fe3e5002c85b1905c4a92f38197a2120a12
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-10-12 00:06:28 +0700

    layout

---
+++ app/src/main/java/id/ac/unand/tb_klp_7/ListSeminarKp.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.tb_klp_7
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListSeminarKp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_seminar_kp)
+    }
+}
