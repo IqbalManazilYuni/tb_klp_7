19254f84cb1724bfbe8c4ea694b5207c54cabf38
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2022-10-13 14:12:42 +0700

    Proggres Sampai tanggal 13 Oktober menambah fragment dan detail logbook mahasiswa

---
+++ app/src/main/java/id/ac/unand/tb_klp_7/LogBookData.kt
@@ -0,0 +1,6 @@
+package id.ac.unand.tb_klp_7
+
+data class datalogbook (
+    val hari:String,
+    val tanggal:String
+)
---
+++ app/src/main/java/id/ac/unand/tb_klp_7/item_logbook.java
@@ -0,0 +1,6 @@
+package id.ac.unand.tb_klp_7;
+
+import android.app.Activity;
+
+public class item_logbook extends Activity {
+}
